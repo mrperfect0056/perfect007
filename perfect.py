9d026d3ee920c670d497c0778225a76c8a2aeda3
mrperfect0056 <87979690+mrperfect0056@users.noreply.github.com>
2021-08-16 15:41:52 +0600

    Create perfect.py

---
+++ perfect.py
@@ -0,0 +1,40 @@
+<h3><b><i>🏆 Github Statistics :</i></b></h3>
+<a href="https://github.com/perfect007-cyber"><img title="trophy" src="https://github-profile-trophy.vercel.app/?username=perfect007-cyber&theme=monokai"></a>
+</p>  
+<p align="center"> 
+ 𝚅𝙸𝚂𝙸𝚃𝙾𝚁𝚂 𝙲𝙾𝚄𝙽𝚃
+ <img src="https://profile-counter.glitch.me/perfect007-cyber/count.svg" />
+</p>
+
+![20200808_160757](https://raw.githubusercontent.com/perfect007-cyber/James404-cyber/main/Screenshot_20210201-204155_Termux.jpg)
+
+![20200808_160757](https://raw.githubusercontent.com/perfect007-cyber/James404-cyber/main/106824690-8dd73a00-66ad-11eb-89e2-53e13ac6f594.gif)
+
+![github stats](https://github-readme-stats.vercel.app/api?username=perfect007-cyber&show_icons=true&include_all_commits=true&theme=chartreuse-dark&cache_seconds=3200)
+
+<img align="center" src="https://github-readme-stats.anuraghazra1.vercel.app/api/top-langs/?username=perfect007-cyber&layout=compact&theme=chartreuse-dark" />
+<p align="center"> 
+
+#Languages and Tools
+</p>
+
+<p align="center">
+<img src="https://raw.githubusercontent.com/8bithemant/8bithemant/master/svg/dev/languages/html.svg" alt="Twitter" style="vertical-align:top; margin:4px"> <img src="https://raw.githubusercontent.com/8bithemant/8bithemant/master/svg/dev/languages/csharp.svg"alt="Twitter" style="vertical-align:top; margin:4px"> <img src="https://raw.githubusercontent.com/8bithemant/8bithemant/master/svg/dev/languages/js.svg" alt="Twitter" style="vertical-align:top; margin:4px"> <img src="https://raw.githubusercontent.com/8bithemant/8bithemant/master/svg/dev/misc/cloud.svg" alt="Twitter" style="vertical-align:top; margin:4px"> <img src="https://raw.githubusercontent.com/8bithemant/8bithemant/master/svg/dev/misc/datascience.svg" alt="Twitter" style="vertical-align:top; margin:4px"> <img src="https://raw.githubusercontent.com/8bithemant/8bithemant/master/svg/dev/services/aws.svg" alt="Twitter" style="vertical-align:top; margin:4px"> <img src="https://raw.githubusercontent.com/8bithemant/8bithemant/master/svg/dev/services/npm.svg" alt="Twitter" style="vertical-align:top; margin:4px"> <img src="https://raw.githubusercontent.com/8bithemant/8bithemant/master/svg/dev/tools/bash.svg" alt="Twitter" style="vertical-align:top; margin:4px">
+ </p>
+<p align="center">
+<code><a href="https://www.python.org/" target="_blank"><img height="50" src="https://www.vectorlogo.zone/logos/python/python-ar21.svg"></a></code>
+<code><a href="https://www.linux.org/" target="_blank"><img height="50" src="https://www.vectorlogo.zone/logos/linux/linux-ar21.svg"></a></code>
+<code><a href="https://reactjs.org/" target="_blank"><img height="50" src="https://www.vectorlogo.zone/logos/reactjs/reactjs-ar21.svg"></a></code>
+<code><a href="https://www.docker.com/" target="_blank"><img height="50" src="https://www.vectorlogo.zone/logos/docker/docker-official.svg"></a></code>
+<br/><br/>
+</p>
+<img align="center" src="https://github-readme-stats.anuraghazra1.vercel.app/api/pin/?username=perfect007-cyber&repo=PROHACK&theme=chartreuse-dark" />
+<p align="center">
+<a href="https://github.com/James404-cyber/PROHACK"><img title="PROHACK" src="https://github-readme-stats.vercel.app/api/pin/?username=perfect007-cyber&repo=PROHACK&theme=vision-friendly-dark"></a>
+<a href="https://github.com/perfect007-cyber/BanglaAll"><img title="BDALL" src="https://github-readme-stats.vercel.app/api/pin/?username=perfect007-cyber&repo=BanglaAll&theme=dark"></a>
+<a href="https://github.com/perfect007-cyber/Number"><img title="NUMBER" src="https://github-readme-stats.vercel.app/api/pin/?username=perfect007-cyber&repo=Number&theme=vision-friendly-dark"></a>
+<a href="https://github.com/perfect007-cyber/Email"><img title="EMAILCRACK" src="https://github-readme-stats.vercel.app/api/pin/?username=perfect007-cyber&repo=Email&theme=tokyonight"></a>
+</p>
+
+
+
